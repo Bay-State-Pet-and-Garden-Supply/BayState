08661b06f225f330ad532d482226b3a2316c38f6
nickborrello <nvborrello@gmail.com>
2026-05-15 00:59:10 -0400

    refactor: remove redundant cohort processing and grouping modules

--- apps/scraper/scrapers/cohort/__init__.py
+++
@@ -1,6 +0,0 @@
-from __future__ import annotations
-
-from .job_processor import CohortJobProcessor, CohortJobResult
-from .processor import CohortProcessor
-
-__all__ = ["CohortJobProcessor", "CohortJobResult", "CohortProcessor"]
